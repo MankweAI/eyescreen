59b3636014389103a6a33f3d2f3ffdbaa8526881
AIDentistry <zettaverse86@gmail.com>
2024-09-25 22:36:30 +0200

    Your commit message describing the changes

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ import BasicForm from "@/components/GoogleForm";
 import ImageSlider from "@/components/ImageSlider";
 import GoogleAnalytics from "@/components/GoogleAnalytics";
 import ChatPopup from "@/components/ChatPopUp";
+import Members from "@/components/Members";
 
 
 export default function Home() {
@@ -15,7 +16,7 @@ export default function Home() {
       <GoogleAnalytics />
       <div className=" w-full flex flex-col justify-center items-center">
         <main className="w-full h-auto  flex flex-col justify-center items-center gap-0">
-          <section className="flex flex-col sm:flex-row justify-between  p-2 sm:p-4 md:p-6 lg:p-8 items-center w-full gap-2 sm:gap-3 md:gap-4 lg:gap-5 xl:gap-6 mt-5 ">
+          <section className="flex flex-col sm:flex-row justify-between  p-2 sm:p-4 md:p-6 lg:p-8 items-center w-full gap-2 sm:gap-3 md:gap-4 lg:gap-5 xl:gap-6 mt-5 bg-sky-200">
             <div className="flex flex-col justify-between items-center sm:w-full md:w-1/2 gap-2 sm:gap-3 md:gap-4 lg:gap-5 xl:gap-6 2xl:gap-7 mt-5 sm:mt-7 md:mt-10 lg:mt-12 xl:mt-15">
               <h1 className="text-3xl sm:text-4xl md:text-4xl lg:text-6xl text-center font-black text-gray-700">
                 Simple, fun and non-diagnostic
@@ -42,7 +43,7 @@ export default function Home() {
             </div>
           </section>
 
-          <section className="flex flex-col sm:flex-row justify-between  p-2 sm:p-4 md:p-6 lg:p-8 items-center w-full gap-2 sm:gap-3 md:gap-4 lg:gap-5 xl:gap-6 mt-5 sm:mt-7 md:mt-10 lg:mt-12">
+          <section className="flex flex-col sm:flex-row justify-between  p-2 sm:p-4 md:p-6 lg:p-8 items-center w-full gap-2 sm:gap-3 md:gap-4 lg:gap-5 xl:gap-6 mt-5 sm:mt-7 md:mt-10 lg:mt-12 ">
             <div className="flex flex-col justify-between items-center sm:w-full gap-2 sm:gap-3 md:gap-4 lg:gap-5 xl:gap-6 2xl:gap-7 mt-5 sm:mt-7 md:mt-10 lg:mt-12 xl:mt-15">
               <h1 className="text-3xl sm:text-4xl md:text-4xl lg:text-5xl text-center font-black text-gray-700 p-2 flex justify-center gap-4">
                 About{" "}
@@ -71,9 +72,8 @@ export default function Home() {
                 />
                 <div className="flex justify-center items-center bg-transparent w-full gap-2 p-2 border-2 rounded-xl">
                   <p className="text-sm sm:text-base md:text-lg lg:text-xl text-start text-gray-700 font-medium whitespace-pre-line w-full">
-                    We achieve this by offering fun quizzes and simple and
-                    non-diagnostic eye pre-screen on your social media platforms
-                    and website
+                    We achieve this by offering fun quizzes and simple, basic
+                    and non-diagnostic eye pre-screens
                   </p>
                 </div>
               </div>
@@ -85,7 +85,7 @@ export default function Home() {
                 />
                 <div className="flex justify-center items-center bg-transparent w-full gap-2 p-2 border-2 rounded-xl">
                   <p className="text-sm sm:text-base md:text-lg lg:text-xl text-start text-gray-700 font-medium whitespace-pre-line w-full">
-                    We send the user`s results to you and ask them to book a
+                    We send the user`s score to you and ask them to book a
                     follow-up
                   </p>
                 </div>
@@ -98,8 +98,7 @@ export default function Home() {
                 />
                 <div className="flex justify-center items-center bg-transparent w-full gap-2 p-2 border-2 rounded-xl">
                   <p className="text-sm sm:text-base md:text-lg lg:text-xl text-start text-gray-700 font-medium whitespace-pre-line w-full">
-                    For those who book, we send weekly eye care tips in your
-                    name (optional)
+                    We send weekly eye care tips in your name (optional)
                   </p>
                 </div>
               </div>
@@ -108,21 +107,61 @@ export default function Home() {
                   style={{ color: "#C60D69" }}
                   className="h-6 w-6"
                 />
-                <div className="flex justify-center items-center bg-transparent w-full gap-2 p-2 border-2 rounded-xl">
-                  <p className="text-sm sm:text-base md:text-lg lg:text-xl text-start text-gray-700 font-medium whitespace-pre-line w-full">
-                    For those who don`t book, we offer to send weekly eye care
-                    tips in your name (optional)
-                  </p>
+                <div className="flex justify-start items-center bg-transparent w-full gap-2 p-2 border-2 rounded-xl">
+                  <div className="flex justify-between items-center bg-transparent w-full gap-2">
+                    {" "}
+                    <p className="text-sm sm:text-base md:text-lg lg:text-xl text-start text-gray-700 font-medium whitespace-pre-line">
+                      We reach your users anywhere
+                    </p>
+                    <Image
+                      src="/images/facebook-icon.svg"
+                      alt="Logo"
+                      width={40}
+                      height={10}
+                      className="inline-block cursor-pointer"
+                    />
+                    <Image
+                      src="/images/whatsapp_icon.svg"
+                      alt="Logo"
+                      width={40}
+                      height={10}
+                      className="inline-block cursor-pointer"
+                    />
+                    <Image
+                      src="/images/instagram-icon.svg"
+                      alt="Logo"
+                      width={40}
+                      height={10}
+                      className="inline-block cursor-pointer"
+                    />
+                    <Image
+                      src="/images/web-icon.svg"
+                      alt="Logo"
+                      width={40}
+                      height={10}
+                      className="inline-block cursor-pointer"
+                    />
+                  </div>
                 </div>
               </div>
             </div>
           </section>
 
-          <section className="flex flex-col sm:flex-col justify-between  p-2 sm:p-4 md:p-6 lg:p-8 items-center w-full gap-2 sm:gap-3 md:gap-4 lg:gap-5 xl:gap-6 mt-5 sm:mt-7 md:mt-10 lg:mt-12">
+          <section className="flex flex-col sm:flex-col justify-between  p-2 sm:p-4 md:p-6 lg:p-8 items-center w-full gap-2 sm:gap-3 md:gap-4 lg:gap-5 xl:gap-6 mt-5 sm:mt-7 md:mt-10 lg:mt-12 bg-sky-100">
             <h1 className="text-3xl sm:text-4xl md:text-4xl lg:text-5xl text-center font-black text-gray-700">
               Quizzes & Tests
             </h1>
             <ImageSlider />
+          </section>
+
+          <section className="flex flex-col sm:flex-col justify-between p-2  items-center w-full gap-2 mt-12 ">
+            <h1 className="text-3xl sm:text-4xl md:text-4xl lg:text-5xl text-center font-black text-gray-700">
+              Our Volunteers
+            </h1>
+            <p className="text-sm sm:text-base md:text-sm lg:text-lg text-center text-gray-700 font-medium whitespace-pre-line w-full md:w-1/2">
+              Click for website
+            </p>
+            <Members />
           </section>
           <section className="flex flex-col sm:flex-col justify-between  p-2 sm:p-4 md:p-6 lg:p-8 items-center w-full gap-2 sm:gap-3 md:gap-4 lg:gap-5 xl:gap-6 mt-5 sm:mt-7 md:mt-10 lg:mt-12">
             <h1 className="text-3xl sm:text-4xl md:text-4xl lg:text-5xl text-center font-black text-gray-700">
@@ -149,6 +188,18 @@ export default function Home() {
           </section>
           <ChatPopup />
         </main>
+
+        <section className="flex flex-col sm:flex-col justify-between p-2 items-center w-full gap-2 mt-4">
+          <div className="inline-block cursor-pointer">
+            <Image
+              src="/images/motto.png"
+              alt="Logo"
+              width={500}
+              height={10}
+              className="w-full sm:w-[300px] md:w-[400px] lg:w-[500px] h-auto"
+            />
+          </div>
+        </section>
       </div>
     </>
   );
